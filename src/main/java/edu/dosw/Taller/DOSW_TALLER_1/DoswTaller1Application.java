496c877786c0032664a79799c9b211e8b53b0740
Ana Fiquitiva <Ana.Fiquitiva-p@mail.esuelaing.edu.co>
2025-09-02 09:09:15 -0500

    Actualización de la estructura de Maven

---
+++ src/main/java/edu/dosw/Taller/DOSW_TALLER_1/DoswTaller1Application.java
@@ -0,0 +1,13 @@
+package edu.dosw.Taller.DOSW_TALLER_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoswTaller1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoswTaller1Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dosw/Taller/DOSW_TALLER_1/DoswTaller1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.Taller.DOSW_TALLER_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoswTaller1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
